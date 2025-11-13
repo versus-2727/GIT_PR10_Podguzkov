b60a46d044b2ba7fea1425d5bc1a2827f204156e
Mudrov Maksim <mudrov010203@gmail.com>
2025-11-13 10:00:22 +0300

    edit program cs

--- qaz/Program.cs
+++ qaz/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine($"Артём Подгузков ИС-23Б");
         Console.WriteLine($"Current date: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
     }
 }
